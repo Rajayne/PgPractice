695b994287dbca4b81756de3578d386c898a6bdb
Rajayne <ryann.j.nealey@gmail.com>
2023-04-20 02:02:31 -0700

    Handle async await errors with try catch

--- VideoCode/routes/users.js
+++ VideoCode/routes/users.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/all', async (req, res, next) => {
-    const allUsers = await db.query(`SELECT * FROM USERS`);
-    return res.json(allUsers.rows);
+    try {
+        const allUsers = await db.query(`SELECT * FROM USERS`);
+        return res.json(allUsers.rows);
+    } catch (e) {
+        return next(e)
+    }
 })
 
 
